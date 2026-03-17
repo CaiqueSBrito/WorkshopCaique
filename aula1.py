baefc967e512737338b14fe12a68cffbcb2b4d3a
Caique Brito <gameroff12345@gmail.com>
2026-03-17 15:11:20 -0300

    exe2_1_1703

--- aula1.py
+++ aula1.py
@@ -1,8 +1,24 @@
-idade = input("Digite sua idade: ")
-idade = int(idade)
-if idade >= 18:
-    print("sua idade está certa: ", idade)
-if idade < 18:
-    print(("sua idade está abaixo"))
-else
-    print("você não digitou um número.")
+saldo = 0
+historico = []
+
+while True:
+
+    print("Banco Fabrica\n 1. Depósito \n 2. Saque \n 3.Extrato \n 4.Sair")
+    resposta = int(input("Selecione: "))
+
+    if resposta == 1:
+        deposito = (int(input("Quanto você quer depositar:  ")))
+        historico.append(f"Você depositou: {deposito}")
+        deposito = deposito + saldo
+
+    elif resposta == 2:
+        saque = int(input("Quanto você quer sacar: "))
+        saldo = saldo - saque
+        historico.append(f"Você depositou: {saque}")
+
+    elif resposta == 3: 
+        for i in range (len(historico)):
+            print(historico[i])
+
+    elif resposta == 4:
+        break
